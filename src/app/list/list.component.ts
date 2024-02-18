d7155afd3cc110b4413c121a612de1c60b1e102a
Lara Newsom <laramnewsom@gmail.com>
2024-02-18 17:38:04 -0600

    setting up libraries

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -11,10 +11,19 @@ import { ColDef } from 'ag-grid-community';
 })
 export class ListComponent {
   rowData = [
-    { designation: "American Dumpster Goblin", name: "Stinky Feets", age: 12, color: 'tabby' },
-    { designation: "Midwest Barn Patrol", name: "Grandma", age: 3, color: 'buff torti' },
-    { designation: "Pacific Couch Slug", name: "Precious", age: 4.5, color: 'orange tabby' },
-    { designation: "Northeast Toilet Inspector", name: "Frank", age: 8, color: 'tuxedo' },
+    { designation: "Dumpster Goblin", name: "Stinky Feets", age: 12, color: 'tabby' },
+    { designation: "Barn Patrol", name: "Grandma", age: 3, color: 'buff torti' },
+    { designation: "Couch Slug", name: "Precious", age: 4.5, color: 'orange tabby' },
+    { designation: "Toilet Inspector", name: "Frank", age: 8, color: 'tuxedo' },
+    { designation: "Trash Panther", name: "Diana", age: 12, color: 'long hair grey' },
+    { designation: "Food Cop", name: "Paul Blart", age: 3, color: 'buff' },
+    { designation: "Princess", name: "Matthew", age: 9, color: 'siamese seal point' },
+    { designation: "Frisky Biscuit", name: "Cazador", age: 10, color: 'longhair orange' },
+    { designation: "Master Biscuit Maker", name: "Doodles", age: 2, color: 'brown tabby' },
+    { designation: "King Zoomies", name: "Flapjack", age: 1, color: 'orange tuxedo' },
+    { designation: "Crop Duster", name: "Bud", age: 5, color: 'shorthair orange' },
+    { designation: "Chatty Cathy", name: "Andromeda", age: 10, color: 'black' },
+
   ];
 
   // Column Definitions: Defines & controls grid columns.
