fe491bbb7fa525f99ed7e9a16a8d4a2e1ab67d9e
Lara Newsom <laramnewsom@gmail.com>
2024-03-02 14:04:46 -0600

    removed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ListComponent } from './list/list.component';
-import { ClockComponent } from './clock/clock.component';
-import { LegacyComponent } from './legacy/legacy.component';
 
 export const routes: Routes = [
   {
@@ -13,16 +10,4 @@ export const routes: Routes = [
     path: 'home',
     component: HomeComponent,
   },
-  {
-    path: 'clock',
-    component: ClockComponent,
-  },
-  {
-    path: 'list',
-    component: ListComponent,
-  },
-  {
-    path: 'legacy',
-    component: LegacyComponent,
-  }
 ];
